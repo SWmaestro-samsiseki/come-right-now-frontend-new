de708464c03273294a345fc4014e75cb5159c25a
JaeHyeok-Han <hjh0633@gmail.com>
2022-10-21 16:50:49 +0900

    Fix: 모노레포 git 테스트

--- packages/store/src/App.tsx
+++ packages/store/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to store2.
+          Edit <code>src/App.tsx</code> and save to store.
         </p>
         <a
           className="App-link"
