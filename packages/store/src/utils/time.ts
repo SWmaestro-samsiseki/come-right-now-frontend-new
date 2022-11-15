96c0d21a26f64e2a1d57bbb12e9b9ca07f7562e6
junho100 <bemodesty306@naver.com>
2022-11-16 00:22:36 +0900

    test

--- packages/store/src/utils/time.ts
+++ packages/store/src/utils/time.ts
@@ -1,5 +1,5 @@
 function calTermTime(time: Date): number {
-  // TODO: 개발이 끝나면 10분으로 변경하기!
+  // TODO: 개발이 끝나면 10분으로 변경하기
   const limit = 3;
   const term = new Date().getTime() - new Date(time).getTime();
   return term < limit * 60000 ? term : 0;
