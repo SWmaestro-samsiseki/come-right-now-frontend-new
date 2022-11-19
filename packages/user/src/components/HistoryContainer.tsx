021d5fb897d6ac940a21be5851b0965f24f157f8
JaeHyeok-Han <hjh0633@gmail.com>
2022-11-19 16:30:36 +0900

    Refactor: [user] 이용내역 컨테이너 컴포넌트 리팩토링

--- packages/user/src/components/HistoryContainer.tsx
+++ packages/user/src/components/HistoryContainer.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import thema from '../styles/thema';
 import useAuthStore from '../stores/authStore';
 import { StoreInfo } from '../interfaces/reservation';
 import { getReservationHistory } from '../apis/reservationAPI';
 import HistoryList from './HistoryList';
+import ListEmpty from './ListEmpty';
 
 const Container = styled.div`
   width: 100%;
@@ -15,28 +15,12 @@ const Container = styled.div`
     display: none;
   }
 `;
-const EmtpyHistory = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  font: ${thema.font.p2};
-  color: ${thema.color.primary.main2_active};
-
-  & img {
-    width: 140px;
-    margin-bottom: 12px;
-  }
-`;
 
 function HistoryContainer() {
   const { auth } = useAuthStore();
   const [historyList, setHistoryList] = useState<{ day: string; store: StoreInfo[] }[]>([]);
-  const [text, setText] = useState('이용내역이 없습니다 :0');
 
-  async function fetchHistory(id: string) {
+  const fetchHistory = async (id: string) => {
     const response = await getReservationHistory(id);
     if (!('error' in response)) {
       setHistoryList(
@@ -49,10 +33,8 @@ function HistoryContainer() {
           };
         }),
       );
-    } else {
-      setText(response.message);
     }
-  }
+  };
 
   useEffect(() => {
     if (auth) {
@@ -62,18 +44,10 @@ function HistoryContainer() {
 
   return (
     <Container>
-      {historyList.length === 0 ? (
-        <EmtpyHistory>
-          <img
-            src={
-              'https://s3.ap-northeast-2.amazonaws.com/static.jigeumgo.com/images/emtpyHistory.png'
-            }
-            alt="빈 이미지"
-          />
-          {text}
-        </EmtpyHistory>
-      ) : (
+      {historyList.length !== 0 ? (
         historyList.map((list, index) => <HistoryList key={index} list={list} />)
+      ) : (
+        <ListEmpty text={'이용내역이 없습니다 :0'} />
       )}
     </Container>
   );
